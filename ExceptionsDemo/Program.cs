2f87f1ea45a156c37a21842745280b145eb0c93f
MICHAEL BAZAN SANCHEZ <mbazansanchez@cnm.edu>
2021-06-25 12:14:18 -0600

    Add project files.

---
+++ ExceptionsDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ExceptionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string doAnother;
+
+            do
+            {
+
+                try
+                {
+                    Console.WriteLine("Please enter num1: ");
+                    string Num1str = Console.ReadLine();
+                    int num1 = int.Parse(Num1str);
+
+                    Console.Write("Please enter num2: ");
+                    string num2str = Console.ReadLine();
+                    int num2 = int.Parse(num2str);
+
+                    Console.WriteLine("num1/num2: {0}", num1 / num2);
+                } catch (System.FormatException exc)
+                {
+                    Console.WriteLine("Not a number");
+                } catch (System.OverflowException exc)
+                {
+                    Console.WriteLine("Number is to large or small!");
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("Error " + exc.Message + "\nException type: " +
+                        exc.GetType());
+
+                } finally
+                {
+                    Console.WriteLine("This section always executes");
+                }
+
+                Console.WriteLine("Do another? (y/n): ");
+                doAnother = Console.ReadLine();
+            } while (doAnother == "y");
+
+        }
+    }
+}
